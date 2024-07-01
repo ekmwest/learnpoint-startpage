62038cf59bcb55649b13bdbcad221283305fc0f3
unknown <dhanfiarahman@gmail.com>
2024-07-01 10:04:24 +0200

    Version A and B adjustments

---
+++ docs/b/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('click', event => {
+
+    const toggleButton = event.target.closest('[data-element="notifications-popover.toggle-button"]');
+    if (!toggleButton) {
+        return;
+    }
+    const notificationsPopover = document.querySelector('[data-component="notifications-popover"]');
+    if(!notificationsPopover) {
+        return;
+    }
+
+    notificationsPopover.classList.toggle('OPEN');  
+  
+});
