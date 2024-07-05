6540748af08cc0c7dd8f38049934ffb25fa6b6f7
unknown <dhanfiarahman@gmail.com>
2024-07-05 13:16:21 +0200

    Version B adding details

--- docs/b/script.js
+++ docs/b/script.js
@@ -12,3 +12,18 @@ document.addEventListener('click', event => {
     notificationsPopover.classList.toggle('OPEN');  
   
 });
+
+document.addEventListener('click', event => {
+
+    const toggleButton = event.target.closest('[data-element="profile-menu.toggle-button"]');
+    if (!toggleButton) {
+        return;
+    }
+    const profileMenu = document.querySelector('[data-component="profile-menu"]');
+    if(!profileMenu) {
+        return;
+    }
+
+    profileMenu.classList.toggle('OPEN');  
+  
+});
